079b3368c9839a2dc67cd8f2356efd09d875fcef
Zamralik <vuk.kulvar+github@gmail.com>
2024-02-16 18:22:17 +0100

    clean config

--- packages/architectura/docker/scripts/boot.sh
+++ packages/architectura/docker/scripts/boot.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 FILE_PATH="$(realpath ${BASH_SOURCE})"
 CURRENT_DIR="$(dirname ${FILE_PATH})"
 
-
 bash "${CURRENT_DIR}"/down.sh && bash "${CURRENT_DIR}"/up.sh
--- packages/architectura/docker/scripts/up.sh
+++ packages/architectura/docker/scripts/up.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 FILE_PATH="$(realpath ${BASH_SOURCE})"
 CURRENT_DIR="$(dirname ${FILE_PATH})"
 
-
 docker-compose -f "${CURRENT_DIR}"/../docker-compose.yml -p vitruvius up -d --build && docker logs -f vitruvius
