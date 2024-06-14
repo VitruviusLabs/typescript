316e35826033647753e67765ffbf8e8d8ed9f54a
Zamralik <vuk.kulvar+github@gmail.com>
2024-06-14 13:39:48 +0200

    chore: eslint config: rename folder and remove unnecessary import

--- .eslint/configuration/_index.mjs
+++ .eslint/configuration/_index.mjs
@@ -1,15 +1,14 @@
-import { config } from "typescript-eslint";
 import { configuration as default_configuration } from "./global/default.mjs";
 import { configuration as ignores_configuration } from "./global/ignores.mjs";
 import { configuration as strict_configuration } from "./strict/config.mjs";
-import { configuration as stylistic_configuration } from "./stylistic/config.mjs";
+import { configuration as style_configuration } from "./style/config.mjs";
 import { configuration as test_configuration } from "./test/config.mjs";
 
 const CONFIGURATIONS = [
 	default_configuration,
 	ignores_configuration,
 	strict_configuration,
-	stylistic_configuration,
+	style_configuration,
 	test_configuration,
 ];
 
